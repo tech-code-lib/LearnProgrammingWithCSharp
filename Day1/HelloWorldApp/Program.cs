efe6f9b13b5896ac096ce6586335c0c8873db74b
Nitin Rastogi <c-nitin.rastogi@chartercom.com>
2021-07-11 13:31:09 -0400

    Day 1 Hello World Application

---
+++ Day1/HelloWorldApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print a Hello Message on output window.
+            Console.WriteLine("Hello World");
+            
+
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();//variable 'name' of data type string
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey();
+        }
+    }
+}
